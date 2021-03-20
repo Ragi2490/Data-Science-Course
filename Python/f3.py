eb741b6ca696c554fb117e933baffe37d3cb5759
Ragi2490 <72399065+Ragi2490@users.noreply.github.com>
2021-03-20 09:32:37 +0530

    Add files via upload

---
+++ Python/f3.py
@@ -0,0 +1,4 @@
+v1 = first string
+v2 = second string
+v1 = temp
+temp = v2
---
+++ Python/f4.py
@@ -0,0 +1,9 @@
+Swapping two variables
+v1 = 'first string'
+v2 = 'second string'
+temp = v1
+v1 = v2
+v2 = temp
+print(v1)
+print(v2)
+
